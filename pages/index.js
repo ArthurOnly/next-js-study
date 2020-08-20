3adf8bb6f4f67e76c925d4ce0b6bc1d583a33f71
Arthur Medeiros Paiva <56327949+ArthurOnly@users.noreply.github.com>
2020-08-20 16:31:46 -0300

    Header styling

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,10 @@ function Home() {
   return (
     <div id="login-page" className="login-page">
       <div className="form">
-        <img src="" className="logo" />
+        <img
+          src="https://suap.ifrn.edu.br/static/comum/img/logo-login.png"
+          className="logo"
+        />
         <div className="form-label">
           <img src="" />
           <h4>Login</h4>
@@ -20,6 +23,32 @@ function Home() {
           height: 100%;
 
           background-color: var(--background-dark);
+          display: flex;
+          flex-direction: column;
+        }
+        .form {
+          background-color: var(--background-light);
+          width: 90%;
+          align-self: center;
+          margin: auto 0;
+          border-radius: var(--border-sm);
+        }
+        .form .logo {
+          padding: 10px;
+          display: block;
+          margin: 0 auto;
+        }
+        .form .form-label {
+          display: flex;
+          flex-direction: row;
+          margin: auto 20px;
+          border-bottom: 1px solid var(--text-in-light);
+          padding-bottom: 0.5rem;
+        }
+        .form .form-label h4 {
+          font-weight: 300;
+          font-size: 1.2rem;
+          color: var(--text-in-light);
         }
       `}</style>
     </div>
