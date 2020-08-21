2bf7bd94db3a92e1ac4814c1b61efe103f6bf9ef
Arthur Medeiros Paiva <56327949+ArthurOnly@users.noreply.github.com>
2020-08-21 14:18:57 -0300

    Index page form finalized

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,19 @@ function Home() {
           <h4>Login</h4>
         </div>
         <form>
-          <div class="input-block"></div>
+          <div className="input-block">
+            <label htmlFor="login">Usuário</label>
+            <input name="login" type="text"></input>
+          </div>
+          <div className="input-block">
+            <label htmlFor="login">Usuário</label>
+            <input name="login" type="text"></input>
+          </div>
+          <button type="submit">Acessar</button>
         </form>
+        <div className="help">
+          <a href="#">Esqueceu ou deseja alterar sua senha?</a>
+        </div>
       </div>
       <aside className="aside-menu"></aside>
       <footer></footer>
@@ -34,6 +45,7 @@ function Home() {
           width: 90%;
           align-self: center;
           margin: auto 0;
+          padding: 45px 0;
           border-radius: var(--border-sm);
         }
         .form .logo {
@@ -45,13 +57,68 @@ function Home() {
           display: flex;
           flex-direction: row;
           margin: auto 20px;
-          border-bottom: 1px solid var(--text-in-light);
+          border-bottom: 1px solid #eee;
           padding-bottom: 0.5rem;
         }
         .form .form-label h4 {
           font-weight: 300;
           font-size: 1.2rem;
+          color: #999;
+        }
+
+        .form form {
+          display: flex;
+          flex-direction: column;
+          margin: auto 20px;
+          margin-top: 1.5rem;
+        }
+
+        .form .input-block {
+          display: flex;
+          flex-direction: column;
+        }
+
+        .form .input-block label {
+          font-size: 0.8rem;
           color: var(--text-in-light);
+          margin-bottom: 0.3rem;
+        }
+        .form .input-block input {
+          height: 2.8rem;
+          border: 2px solid var(--text-in-label);
+          border-radius: var(--border-sm);
+          padding: 0 0.8rem;
+        }
+        .form .input-block input:focus {
+          border: 2px solid var(--primary);
+          outline: 0;
+        }
+        .form .input-block + .input-block {
+          margin-top: 1rem;
+        }
+
+        .form form button {
+          margin-top: 1rem;
+          border-radius: 2rem;
+          height: 2.8rem;
+          background-color: var(--primary);
+          color: #fff;
+          font-size: 1rem;
+          font-weight: 300;
+        }
+        .form form button:hover {
+          background-color: var(--primary-hover);
+        }
+        .form .help {
+          margin-top: 1rem;
+          display: flex;
+          justify-content: flex-end;
+        }
+        .form a {
+          font-size: 0.8rem;
+          color: var(--primary);
+          margin: auto 20px;
+          align-self: flex-end;
         }
       `}</style>
     </div>
